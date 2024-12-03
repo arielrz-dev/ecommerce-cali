45bc5469216c0e4f465f696bffc9b0bfc386e513
arielrz-dev <ruizarmando.dev@gmail.com>
2024-12-03 10:06:34 -0300

    added mongo-pipe

--- src/products/entities/Product.entity.ts
+++ src/products/entities/Product.entity.ts
@@ -1,9 +1,36 @@
-export class Product {
-  id: number;
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema()
+export class Product extends Document {
+  // @Prop({ required: false, type: Number })
+  // id: number;
+
+  @Prop({ required: true, type: String, maxlength: 50 })
   name: string;
+
+  @Prop({ required: false, type: String, maxlength: 255 })
   description: string;
+
+  @Prop({ type: Number })
   price: number;
+
+  @Prop({ type: Number })
   stock: number;
+
+  @Prop({ type: String, maxlength: 70 })
   made_in: string;
+
+  @Prop({ type: String })
   image: string;
+
+  @Prop({ type: Date, nullable: true, default: null })
+  deletedAt?: Date;
+
+  @Prop({ type: Date, nullable: true, default: null })
+  createdAt?: Date;
+
+  @Prop({ type: Date, nullable: true, default: null })
+  updateAt?: Date;
 }
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -5,8 +5,18 @@ import { CategoriesService } from './services/categories.service';
 import { ManufacturersService } from 'src/products/services/manufacturers.service';
 import { ManufacturersController } from 'src/products/controllers/manufacturers.controller';
 import { CategoriesController } from 'src/products/controllers/categories.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductSchema } from './entities/Product.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Product',
+        schema: ProductSchema,
+      },
+    ]),
+  ],
   controllers: [
     ProductsController,
     ManufacturersController,
