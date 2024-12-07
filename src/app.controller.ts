f14c07b3981982fa66eebaa276ec54c95e4edf4c
arielrz-dev <ruizarmando.dev@gmail.com>
2024-12-06 23:54:51 -0300

    added test guard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Inject,
+  SetMetadata,
+  UseGuards,
+} from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import config from './config';
 import { AppService } from './app.service';
 import { ApiExcludeController } from '@nestjs/swagger';
 import { Db } from 'mongodb';
+import { ApiKeyGuard } from './auth/guards/api-key.guard';
+import { Public } from './auth/decorators/public.decorator';
 
 @ApiExcludeController()
 @Controller()
+@UseGuards(ApiKeyGuard)
 export class AppController {
   constructor(
     private readonly appService: AppService,
@@ -19,7 +28,18 @@ export class AppController {
     return this.appService.getUseFactory();
   }
 
-  @Get('tasks')
+  @Get('/guards')
+  getHello(): string {
+    return 'method to test Guardians';
+  }
+
+  @SetMetadata('isPublic', true)
+  @Get('/public')
+  getPublic(): string {
+    return 'route Public';
+  }
+  @Public()
+  @Get('/tasks')
   tasks() {
     const tasksCollection = this.database.collection('tasks');
     return tasksCollection.find().toArray();
